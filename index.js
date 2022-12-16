25ca0a11d22d07bc3784bc99fd12b411e39795c3
ryannarnoldd <ryannarnoldd@gmail.com>
2022-12-16 14:51:47 -0500

    Custom status and message replies (dictionary)

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, Presence } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+message_replies = {'yee': 'haw!', 'ping': 'pong!', 'hi': 'hello!', 
+                   'hello': 'hi!', 'hey': 'hi!', 'yo': 'hey!',
+                   'how are you?': 'I am good'}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ 
+        activities: [{ name: 'Da Bible', type: ActivityType.Listening }] });
 })
 
 client.on('messageCreate', async (message) => {
-    if (message.content === 'ping') {
-        message.reply('Pong!');
-    }
-    else if (message.content === 'yee') {
-        message.reply('haw!');
+    if (message.author.bot) return;
+
+    if (message.content.toLowerCase() in message_replies) {
+        message.reply(message_replies[message.content.toLowerCase()]);
     }
 })
 
